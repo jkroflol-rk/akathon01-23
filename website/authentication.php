f0677570658b70e4e5157acd9bd608e219ba33f5
khanhdnk <dangnamkhanhkdgv@gmail.com>
2023-05-13 18:44:36 +0700

    update

---
+++ website/authentication.php
@@ -0,0 +1,38 @@
+<?php
+    require_once("./functions/functions.php");
+
+    $url = "https://b81155ba-05ce-415b-9ca4-b83d935e46a6-asia-south1.apps.astra.datastax.com/api/rest/v2/keyspaces/test/users/rows";
+    $token = "AstraCS:PXhWiFwCPFWfmLXqOGtkOlCU:ef2043b13fcc33dd3e63368eabf3a4379cf561fda6dec8ae2490832acde2ab39";
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json",
+    "Accept: application/json",
+    "X-Cassandra-Token: $token"
+    ));
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $data = json_decode($response);
+    if (isset($_POST["username"]) && isset($_POST["password"])) {
+        $username = sanitise_input($_POST["username"]);
+        $password = sanitise_input($_POST["password"]);
+    }
+?>
+
+<?php 
+  foreach ($data->data as $row) {
+    if (($password == $row->password) && ($username == $row->username && ($row->admin == "true"))) {
+      header('Location: admin.php');
+      exit();
+    } else if (($password == $row->password) && ($username == $row->username)) {
+      header('Location: dashboard.php');
+      exit();
+    }else{
+      header('Location: login.php');
+        
+    }
+  }
+?>
--- website/login.php
+++ website/login.php
@@ -1,49 +1,3 @@
-<?php
-    require_once("./functions/functions.php");
-
-    $url = "https://b81155ba-05ce-415b-9ca4-b83d935e46a6-asia-south1.apps.astra.datastax.com/api/rest/v2/keyspaces/test/users/rows";
-    $token = "AstraCS:PXhWiFwCPFWfmLXqOGtkOlCU:ef2043b13fcc33dd3e63368eabf3a4379cf561fda6dec8ae2490832acde2ab39";
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    "Content-Type: application/json",
-    "Accept: application/json",
-    "X-Cassandra-Token: $token"
-    ));
-    $response = curl_exec($ch);
-    curl_close($ch);
-    $pass = "jane.doe@example.com";
-    echo $pass;
-
-    $data = json_decode($response);
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-?>
-
-<?php foreach ($data->data as $row): ?>
-    <tr>
-      <td><?= $row->id ?></td>
-      <td><?= $row->email ?></td>
-      <td><?= $row->firstname ?></td>
-      <td><?= $row->lastname ?></td>
-      <td><?= $row->username ?></td>
-      <td><?= $row->password ?></td>
-
-      <?php
-        if (($password == $row->password) && ($username == $row->username && ($row->admin == "true"))){
-          header('Location: admin.php');
-        }else if (($password == $row->password) && ($username == $row->username)){
-          echo $row->username;
-          header('Location: dashboard.php');
-        }
-
-      ?>
-    </tr>
-<?php endforeach; ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -86,7 +40,7 @@
         
         <main>
             <div class="login_space">
-                <form action="login.php" method="POST" class="animate__animated animate__fadeInRight">
+                <form action="authentication.php" method="POST" class="animate__animated animate__fadeInRight">
                     <div class="loginform">
                         <h1>Login</h1>
                         <form method="post" action="authentication.php">
