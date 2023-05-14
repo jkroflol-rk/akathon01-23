b1fcc11f8b6a5e8eb09390c054578a05e6f35a34
khanhdnk <dangnamkhanhkdgv@gmail.com>
2023-05-14 16:36:45 +0700

    update

--- website/Upload.php
+++ website/Upload.php
@@ -2,10 +2,10 @@
     session_start();
     require_once("./functions/functions.php");
 
-    function checkprofile(){
-        if (isset($_POST["fname"]) && $_POST["fname"])) {
-            $username = sanitise_input($_POST["username"]);
-            $password = sanitise_input($_POST["password"]);
-        }
-    }
+    // function checkprofile(){
+    //     if (isset($_POST["fname"]) && $_POST["fname"])) {
+    //         $username = sanitise_input($_POST["username"]);
+    //         $password = sanitise_input($_POST["password"]);
+    //     }
+    // }
 ?>
