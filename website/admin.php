fe4cdf19256deb1100b07264c35e12c7bbcdbbcf
rt-kc <tori.tuan@gmail.com>
2023-05-15 23:18:01 +0700

    -admin data

--- website/admin.php
+++ website/admin.php
@@ -6,25 +6,12 @@ if (isset($_SESSION['admin']) && $_SESSION['admin']) {
 } else {
 	header('Location: ./login.php');
 }
-?>
 
-<?php
+include('./conn/func.php');
 
-$url = "https://b81155ba-05ce-415b-9ca4-b83d935e46a6-asia-south1.apps.astra.datastax.com/api/rest/v2/keyspaces/test/users/rows/";
-$token = "AstraCS:PXhWiFwCPFWfmLXqOGtkOlCU:ef2043b13fcc33dd3e63368eabf3a4379cf561fda6dec8ae2490832acde2ab39";
+$data = getData("users");
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-	"Content-Type: application/json",
-	"Accept: application/json",
-	"X-Cassandra-Token: $token"
-));
-$response = curl_exec($ch);
-curl_close($ch);
-
-$data = json_decode($response);
+$d_data = getData("devices");
 ?>
 
 <!DOCTYPE html>
