dda449e7be37d186c0aa60786ea10fbea77d7a91
rt-kc <tori.tuan@gmail.com>
2023-05-16 22:26:01 +0700

    admincomplete?

--- website/admin.php
+++ website/admin.php
@@ -2,7 +2,7 @@
 //if logged in, go to manager page
 session_start();
 if (isset($_SESSION['admin']) && $_SESSION['admin']) {
-	
+
 } else {
 	header('Location: ./login.php');
 }
@@ -14,10 +14,10 @@ $data = getData("users");
 $d_data = getData("devices");
 
 if (isset($_GET['order_id'])) {
-    $del_id = $_GET['order_id'];
-    $delete = deleteData("users", $del_id);
-    echo $delete;
-    header('Location: ./admin.php');
+	$del_id = $_GET['order_id'];
+	$delete = deleteData("users", $del_id);
+	echo $delete;
+	header('Location: ./admin.php');
 }
 ?>
 
@@ -58,14 +58,14 @@ if (isset($_GET['order_id'])) {
 						<li><a href="dashboard.php">Dashboard</a></li>
 						<li><a href="ourteam.php">Our Team</a></li>
 						<?php
-							if (isset($_SESSION['admin']) && $_SESSION['admin']) {
-								echo "<li class='active'><a href='admin.php'>Admin</a></li>";
-							}
-							if ((isset($_SESSION['authenticated']) && $_SESSION['authenticated']) || (isset($_SESSION['admin']) && $_SESSION['admin'])) {
-								echo "<li><a href='logout.php'>Logout</a></li>";
-							} else {
-								echo "<li><a href='login.php'>Login</a></li>";
-							}
+						if (isset($_SESSION['admin']) && $_SESSION['admin']) {
+							echo "<li class='active'><a href='admin.php'>Admin</a></li>";
+						}
+						if ((isset($_SESSION['authenticated']) && $_SESSION['authenticated']) || (isset($_SESSION['admin']) && $_SESSION['admin'])) {
+							echo "<li><a href='logout.php'>Logout</a></li>";
+						} else {
+							echo "<li><a href='login.php'>Login</a></li>";
+						}
 						?>
 					</ul>
 
@@ -78,22 +78,32 @@ if (isset($_GET['order_id'])) {
 			<div class="topology">
 				<h1>Manage Order</h1>
 				<div class="hello_container">
-					<?php foreach ($data->data as $row) : ?>
+					<?php foreach ($data->data as $row): ?>
 						<div class="order-card hello">
-							<h2 class="customer-name">Name: <?= $row->lastname?> <?=$row->firstname?></h2>
-							<p class="ordered-time">Ordered Time: <?= $row->order_time?></p>
-							<p class="num-vlans">Number of Vlans <?= $row->vlan?></p>
-							<p class="num-vlans">Username: <?= $row->username?></p>
-							<p class="num-vlans">Password: <?= $row->password?></p>
+							<h2 class="customer-name">Name:
+								<?= $row->lastname ?> 	<?= $row->firstname ?>
+							</h2>
+							<p class="ordered-time">Ordered Time:
+								<?= $row->order_time ?>
+							</p>
+							<p class="num-vlans">Number of Vlans
+								<?= $row->vlan ?>
+							</p>
+							<p class="num-vlans">Username:
+								<?= $row->username ?>
+							</p>
+							<p class="num-vlans">Password:
+								<?= $row->password ?>
+							</p>
 							<ul class="host-list">
-								<?php 
-									$array = $row->vlans;
-									foreach ($array as $dep) {
-										echo "<li>" . $dep->key . ": " . $dep->value . "</li>";
-									}									
+								<?php
+								$array = $row->vlans;
+								foreach ($array as $dep) {
+									echo "<li>" . $dep->key . ": " . $dep->value . "</li>";
+								}
 								?>
 							</ul>
-							<a href="admin.php?order_id=<?= $row->id?>" class="test_btn">Click to Delete</a>
+							<a href="admin.php?order_id=<?= $row->id ?>" class="test_btn">Click to Delete</a>
 
 						</div>
 
@@ -103,39 +113,51 @@ if (isset($_GET['order_id'])) {
 			</div>
 
 			<div class="device_info">
-				<?php foreach ($d_data->data as $d_row) : ?>
-				
+				<?php foreach ($d_data->data as $d_row): ?>
+
 					<div class="item-box product-box">
 
-						<img src="<?= $d_row->image?>" alt="Item image 1">
-						<h2><?= $d_row->name  ?></h2>
+						<img src="<?= $d_row->image ?>" alt="Item image 1">
+						<h2>
+							<?= $d_row->name ?>
+						</h2>
 
 						<div class="extra-content">
 							<table>
 								<tr>
 									<th>Size</th>
-									<td><?= $d_row->size  ?></td>
+									<td>
+										<?= $d_row->size ?>
+									</td>
 								</tr>
 								<tr>
 									<th>Weight</th>
-									<td><?= $d_row-> weight ?></td>
+									<td>
+										<?= $d_row->weight ?>
+									</td>
 								</tr>
 								<tr>
 									<th>Price</th>
-									<td><?= $d_row->price  ?></td>
+									<td>
+										<?= $d_row->price ?>
+									</td>
 								</tr>
 								<tr>
 									<th>Port</th>
-									<td><?= $d_row->port  ?></td>
+									<td>
+										<?= $d_row->port ?>
+									</td>
 								</tr>
 								<tr>
 									<th>Bandwidth</th>
-									<td><?= $d_row->bandwidth  ?></td>
+									<td>
+										<?= $d_row->bandwidth ?>
+									</td>
 								</tr>
-							
+
 							</table>
 							<br>
-							<a href="<?= $d_row->link?>" target="_blank">More info</a>
+							<a href="<?= $d_row->link ?>" target="_blank">More info</a>
 						</div>
 
 					</div>
@@ -145,7 +167,6 @@ if (isset($_GET['order_id'])) {
 
 			<div class="add_device">
 				<h2>developers rating</h2>
-				<form action="delete.php" method="POST">
 
 					<table>
 						<thead>
@@ -161,41 +182,45 @@ if (isset($_GET['order_id'])) {
 								<!-- <th>Weight</th> -->
 							</tr>
 						</thead>
-	
+
 						<tbody>
-							<?php foreach ($d_data->data as $d_row) : ?>
+							<?php foreach ($d_data->data as $d_row): ?>
 								<tr>
 
-									<td><?=$d_row->id?></td>
-									<td><?=$d_row->name?></td>
-									<td><?=$d_row->port?></td>
-									<td><?=$d_row->bandwidth?></td>
-									<td><?=$d_row->price?></td>
-									<td><?=$d_row->size?></td>
-									<td><?=$d_row->weight?></td>
+									<td><?= $d_row->id ?></td>
+									<td><?= $d_row->name ?></td>
+									<td><?= $d_row->port ?></td>
+									<td><?= $d_row->bandwidth ?></td>
+									<td><?= $d_row->price ?></td>
+									<td><?= $d_row->size ?></td>
+									<td><?= $d_row->weight ?></td>
 									<td>
-										<input type="submit" value="Delete" class="button_two">
+										<a class="button_two" href="updatedevices.php?device_id=<?= $d_row->id ?>" onclick="return confirm('Are you sure you want to delete this device?')">Delete</a>
 									</td>
 								</tr>
 							<?php endforeach; ?>
-							<tr>
-								<td><p>Please fill all the field:</p></td>
-								<td><input type="text" name="lastName"></td>
-								<td><input type="email" name="email"></td>
-								<td><input type="email" name="email"></td>
-								<td><input type="email" name="email"></td>
-								<td><input type="email" name="email"></td>
-								<td><input type="email" name="email"></td>
-								<td>
-									<input type="submit" value="Submit" class="button_one">
-								</td>
-							</tr>
-							
-						
-							
+							<form action="updatedevices.php" method="POST">
+								<tr>
+									<td>
+										<p>Add a device &#40;Dont leave any blank field&#41;</p>
+									</td>
+									<td><input type="text" name="name" required></td>
+									<td><input type="text" name="port" required></td>
+									<td><input type="text" name="bandwidth" required></td>
+									<td><input type="text" name="price" required></td>
+									<td><input type="text" name="size" required></td>
+									<td><input type="text" name="weight" required></td>
+									
+									<td>
+										<input type="submit" value="Submit" class="button_one">
+									</td>
+								</tr>
+							</form>
+
+
+
 						</tbody>
 					</table>
-				</form>
 			</div>
 
 
---
+++ website/updatedevices.php
@@ -0,0 +1,34 @@
+<?php
+include('./conn/func.php');
+
+if (
+    isset($_POST['name']) &&
+    isset($_POST['port']) &&
+    isset($_POST['bandwidth']) &&
+    isset($_POST['price']) &&
+    isset($_POST['size']) &&
+    isset($_POST['weight'])
+) {
+    $ndata = [
+		"id" => uuid(),
+        "name" => $_POST['name'],
+        "image" => "images/placeholder.png",
+        "link" => "https://www.cisco.com/site/us/en/products/networking/switches/index.html",
+        "port" => $_POST['port'],
+        "bandwidth" => $_POST['bandwidth'],
+        "price" => $_POST['price'],
+        "size" => $_POST['size'],
+        "weight" => $_POST['weight']
+	];
+	$showndata =  postData("devices", $ndata);
+	echo $showndata;
+	header('Location: ./admin.php');
+} 
+
+if (isset($_GET['device_id'])) {
+	$deleteid = $_GET['device_id'];
+	$delete = deleteData("devices", $deleteid);
+    echo $delete;
+	header('Location: ./admin.php');
+}
+?>
