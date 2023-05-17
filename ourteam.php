b51d4de86da70c794c5676301de557120efcae42
khanhdnk <dangnamkhanhkdgv@gmail.com>
2023-05-17 13:45:46 +0700

    ourteam

--- ourteam.php
+++ ourteam.php
@@ -16,7 +16,7 @@ session_start();
 
 	<script src="js/form.js"></script>
 	<link rel="stylesheet" href="style/form.css">
-	<title>Ourteam</title>
+	<title>Our Team</title>
 </head>
 
 <body id="ourteam">
